cd5d83b715479e4df5c14ab95750c4903b47f117
Pat Sluth <pat.sluth@gmail.com>
2016-05-28 13:38:17 -0600

    revert primes

--- ProjectEuler/src/Library/hashMap.h
+++ ProjectEuler/src/Library/hashMap.h
@@ -1,10 +1,10 @@
-// 
+//
 //  hashMap.cpp
 //  ProjectEuler
-// 
+//
 //  Created by Pat Sluth on 2015-12-09.
 //  Copyright © 2015 Pat Sluth. All rights reserved.
-// 
+//
 
 #ifndef hashMap_h
 #define hashMap_h
@@ -22,42 +22,52 @@ using namespace std;
 template <class K, class V>
 class hashMap
 {
-    
+	
 private:
-    
-    map<K, V> data;
-    
+	
+	map<K, V> data;
+	
 public:
-    
-    uint64_t size()
-    {
-        return this->data.size();
-    }
-    
-    typename map<K, V>::iterator begin()
-    {
-        return this->data.begin();
-    }
-    
-    typename map<K, V>::iterator rbegin()
-    {
-        return this->data.rbegin();
-    }
-    
-    typename map<K, V>::iterator end()
-    {
-        return this->data.end();
-    }
-    
-    V *valueForKey(K key)
-    {
-        if (this->data.count(key) == false) {
-            this->data[key] = *new V();
-        }
-        
-        return &this->data[key];
-    }
-    
+	
+	uint64_t size()
+	{
+		return this->data.size();
+	}
+	
+	typename map<K, V>::iterator begin()
+	{
+		return this->data.begin();
+	}
+	
+	typename map<K, V>::reverse_iterator rbegin()
+	{
+		return this->data.rbegin();
+	}
+	
+	typename map<K, V>::iterator end()
+	{
+		return this->data.end();
+	}
+	
+	typename map<K, V>::reverse_iterator rend()
+	{
+		return this->data.rend();
+	}
+	
+	typename map<K, V>::iterator find (const K key)
+	{
+		return this->data.find(key);
+	}
+	
+	V *valueForKey(K key)
+	{
+		if (this->data.count(key) == false) {
+			this->data[key] = *new V();
+		}
+		
+		return &this->data[key];
+	}
+	
 };
 
 #endif /* hashMap_h */
